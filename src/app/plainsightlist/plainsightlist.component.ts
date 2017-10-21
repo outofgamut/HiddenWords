71b1a4468efc57eb5a07ce5619f37160b6bcb041
sajeetharan <sajeetharan@outlook.com>
2017-10-21 07:45:22 +0530

    (Feat) : #3 Adding components
    Updated Material modules

---
+++ src/app/plainsightlist/plainsightlist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-plainsightlist',
+  templateUrl: './plainsightlist.component.html',
+  styleUrls: ['./plainsightlist.component.css']
+})
+export class PlainsightlistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
